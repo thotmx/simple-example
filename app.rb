59d972bc42f1d4d92a3560549b75fbfe02de631c
Hermes Ojeda Ruiz <hermes.o.r@gmail.com>
2016-05-24 10:41:54 -0500

    Getting API response

--- app.rb
+++ app.rb
@@ -1,6 +1,10 @@
 require 'sinatra'
+require 'json'
+require 'open-uri'
 
 get '/' do
-    "Hello World!"
+  url = "http://rest-service.guides.spring.io/greeting"
+  response = JSON.parse(open(url).read) 
+  "<h1>#{response["id"]}</h1><h2>#{response["content"]}</h2> "
 end
 
